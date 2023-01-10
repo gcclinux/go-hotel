b0d41aff7e4ee99d8c9220ae5fec4c8bac791f63
Ricardo Wagemaker <java@wagemaker.co.uk>
2023-01-10 21:57:12 +0000

    Completed lesson 40

--- cmd/web/route.go
+++ cmd/web/route.go
@@ -11,7 +11,10 @@ import (
 
 func routes(app *config.AppConfig) http.Handler {
 	mux := chi.NewRouter()
+
 	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
 	return mux
