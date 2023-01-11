ed01fd24e07e0d3b7e48c9baad90329efd0d4299
Ricardo Wagemaker <java@wagemaker.co.uk>
2023-01-11 14:04:32 +0000

    Completed lesson 41

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,14 +6,29 @@ import (
 	"myapp/pkg/handlers"
 	"myapp/pkg/render"
 	"net/http"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the main application function
 func main() {
 
-	var app config.AppConfig
+	// Change this to true if in prodution
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = false // false = cooky goes when browser closes
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
 
 	myTemplateCache, err := render.CreateTemplateCache()
 	if err != nil {
