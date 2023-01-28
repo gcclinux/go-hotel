4c1c6ecf02428ec81aa57fc50e606d463446891f
Ricardo Wagemaker <java@wagemaker.co.uk>
2023-01-28 16:22:22 +0000

    Sat, Jan 28, 2023  4:22:22 PM

--- cmd/web/route.go
+++ cmd/web/route.go
@@ -22,8 +22,10 @@ func routes(app *config.AppConfig) http.Handler {
 	mux.Get("/majors-suite", handlers.Repo.Major)
 	mux.Get("/contact", handlers.Repo.Contact)
 	mux.Get("/search-availability", handlers.Repo.Availability)
-	mux.Post("/search-availability", handlers.Repo.PostAvailability)
 	mux.Get("/make-reservation", handlers.Repo.MkReservation)
+
+	mux.Post("/search-availability", handlers.Repo.PostAvailability)
+	mux.Get("/search-availability-json", handlers.Repo.AvailabilityJSON)
 
 	fileServer := http.FileServer(http.Dir("./static/"))
 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
