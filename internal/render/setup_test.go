858728f34db9e7e7a56de3484c1ac330f4ed40be
Ricardo Wagemaker <java@wagemaker.co.uk>
2023-02-08 16:03:29 +0000

    Completed lesson 76

---
+++ internal/render/setup_test.go
@@ -0,0 +1,37 @@
+package render
+
+import (
+	"encoding/gob"
+	"myapp/internal/config"
+	"myapp/internal/models"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+
+	// Store data that we will be keeping in the session.
+	gob.Register(models.Reservation{})
+
+	// Change this to true if in prodution
+	testApp.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = false // false = cooky goes when browser closes
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	testApp.Session = session
+
+	app = &testApp
+
+	os.Exit(m.Run())
+}
